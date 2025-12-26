b2a1e00291646c191a2da71d49cef1accffb2179
Azzu1997 <silar.basha.ik@gmail.com>
2025-12-26 15:46:38 +0530

    Update Demo_1.java

--- src/test/java/Demo/Demo_1.java
+++ src/test/java/Demo/Demo_1.java
@@ -5,7 +5,12 @@ public class Demo_1 {
 	public static void main(String[] args) {
 		
 		System.out.println(" --- Print ---");
+				System.out.println(" --- kavya happy chrismas and ---");
+				System.out.println(" --- happy newyear in advance ---");
+
+
 		
 	}
 
 }
+
